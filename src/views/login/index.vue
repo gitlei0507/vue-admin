b98a74314cf4f43514306fef29b6fdc529a6d8bd
gitlei0507 <57185557+gitlei0507@users.noreply.github.com>
2026-02-20 20:09:18 +0800

    test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,10 +6,22 @@
             </template>
             <el-form :model="loginForm">
                 <el-form-item>
-                    <el-input v-model="loginForm.uid" placeholder="用户名" />
+                    <el-input v-model="loginForm.uid" placeholder="用户名">
+                        <template #prefix>
+                            <el-icon>
+                                <User />
+                            </el-icon>
+                        </template>
+                    </el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-input v-model="loginForm.password" type="password" placeholder="密码" show-password />
+                    <el-input v-model="loginForm.password" type="password" placeholder="密码" show-password>
+                        <template #prefix>
+                            <el-icon>
+                                <Lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
                 </el-form-item>
                 <el-button type="primary" class="w-full" @click="handleLogin">登录</el-button>
             </el-form>
